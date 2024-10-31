19d388a3533d1ce5de038b6882a257911f4c6661
Nicole Riera <150852807+NicoRi5@users.noreply.github.com>
2024-10-30 20:33:41 -0400

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
-import { useState } from 'react'
-import './App.css'
-import {puppyList} from './data.js'
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+import { puppyList } from "./data.js";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: ", puppyList);
+  const handleClick = (puppyId) => {
+    setFeatPupId(puppyId);
+  };
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log("Featured Pup: ", featuredPup);
 
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
-
-
-export default App
+export default App;
